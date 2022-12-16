8149cffd19de5995dc2c036dc7d27870e6aeee2e
Michael agbo <Michael.e.agbo.work@gmail.com>
2022-12-16 21:06:03 +0000

    Added PassengerSuccessTest class and updated pom file with relevant dependencies for the project

---
+++ src/test/java/testclasses/PassengerSuccessTest.java
@@ -0,0 +1,13 @@
+package testclasses;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PassengerSuccessTest {
+
+
+
+}
