ccd3e82c2d3e072351bbaf2916ca824d627d6aaf
Michael agbo <Michael.e.agbo.work@gmail.com>
2022-12-16 21:08:11 +0000

    added before each setup for tests

--- src/test/java/testclasses/PassengerSuccessTest.java
+++ src/test/java/testclasses/PassengerSuccessTest.java
@@ -1,5 +1,5 @@
 package testclasses;
-
+import org.example.Passenger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,6 +7,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PassengerSuccessTest {
+    private Passenger myPassenger;
+    @BeforeEach
+    void setUp() {
+        myPassenger = new Passenger("Mr", "John Doe", 1234334567891L, 1233456789, 42);
+    }
+
 
 
 
