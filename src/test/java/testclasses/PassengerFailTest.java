6e217091206ea2a169cae992d700d84fdea64bc0
Michael agbo <Michael.e.agbo.work@gmail.com>
2022-12-16 21:19:07 +0000

    Added fail test for ID

--- src/test/java/testclasses/PassengerFailTest.java
+++ src/test/java/testclasses/PassengerFailTest.java
@@ -24,4 +24,10 @@ class PassengerFailTest {
         Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mr", "ls", 1235422173822L, 1222222223, 19);});
         assertEquals("This is not a valid name.", exMessage.getMessage());
     }
+
+    @Test
+    void testIdFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mr", "John Doe", 123542, 1222222223, 19);});
+        assertEquals("This is not a valid ID.", exMessage.getMessage());
+    }
 }
