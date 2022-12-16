743fbadbcd650f794c7973925f1e65e2b0db3a2d
Michael agbo <Michael.e.agbo.work@gmail.com>
2022-12-16 20:44:17 +0000

    Created Passenger variables and constructor

--- src/main/java/org/example/Passenger.java
+++ src/main/java/org/example/Passenger.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class Passenger {
+
+    private String title;
+    private String name;
+    private long id;
+    private long phone;
+    private int age;
+
+    public Passenger(String title, String name, long id, long phone, int age) {
+        setTitle(title);
+        setName(name);
+        setId(id);
+        setPhone(phone);
+        setAge(age);
+    }
 }
